854a275b7e4f871312658ccb72332145daab1cf1
Guilherme FGR <39571461+Guifgr@users.noreply.github.com>
2019-08-26 15:35:37 -0300

    Add files via upload

---
+++ medias.py
@@ -0,0 +1,7 @@
+x = int(input("Nota1\n"))
+y = int(input("Nota2\n"))
+w = int(input("Nota3\n"))
+z = int(input("Nota4\n"))
+resul = (x+y+z+w)/4;
+
+print("A média aritmética é",resul)
---
+++ quadrado.py
@@ -0,0 +1,4 @@
+x = int(input("Quadrado \n"))
+y = x*x;
+x = x*4;
+print("perímetro:",x,"- área",y)
