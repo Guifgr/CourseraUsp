f52dec2a866516043b67e990c593c5f9d7773b3f
Guifgr <guifgr@hotmail.com>
2019-10-02 16:32:03 -0300

    defined functions

--- sabor-da-galera.py
+++ sabor-da-galera.py
@@ -1,68 +1,85 @@
-correto = False
+def pessoas():
+    correto = False
 
-while correto == False:
-    pessoas = int(input('Quantas pessoas vão comer?\n'))
-    correto = input("Você digitou: "+str(pessoas)+" pessoas está correto?\n")
-    correto = correto.lower()
-    if correto == 's' or correto == 'sim' or correto == 'y' or correto == 'yes':
-        correto = True
-    else:
-        correto = False
+    while correto == False:
+        pessoas = int(input('Quantas pessoas vão comer?\n'))
+        correto = input("Você digitou: "+str(pessoas)+" pessoas está correto?\n")
+        correto = correto.lower()
+        if correto == 's' or correto == 'sim' or correto == 'y' or correto == 'yes':
+            correto = True
+        else:
+            correto = False
+        return pessoas
 
-correto = False
+def sabores():
+    correto = False        
+    while correto == False:
+        sabores = int(input('Quantos sabores tem?\n'))
+        correto = input("Você digitou: "+str(sabores)+" sabores está correto?\n")
+        correto = correto.lower()
+        if correto == 's' or correto == 'sim' or correto == 'y' or correto == 'yes':
+            correto = True
+        else:
+            correto = False
+    return sabores
 
-while correto == False:
-    sabores = int(input('Quantos sabores tem?\n'))
-    correto = input("Você digitou: "+str(sabores)+" sabores está correto?\n")
-    correto = correto.lower()
-    if correto == 's' or correto == 'sim' or correto == 'y' or correto == 'yes':
-        correto = True
-    else:
-        correto = False
-correto = False
+def nomeSabores(comidas):
+    flavor = [0] * comidas
+    correto = False      
 
-flavor = [''] * sabores
-tempflavor = [0]*sabores
-final = tempflavor[:]
-while correto == False:
-    for i in range(sabores):
-        flavor[i] = input("Qual o sabor numero: "+str(i+1)+"?: ")
+    while correto == False:
+        for i in range(comidas):
+            flavor[i] = input("Qual o sabor numero: "+str(i+1)+"?: ")
 
-    print("\n\nOs sabores são:")
+        print("\n\nOs sabores são:")
 
-    for i in range(sabores):
-        print(str(flavor[i])+"")
+        for i in range(comidas):
+            print(str(flavor[i])+"")
 
-    print("\n")
+        print("\n")
+        
+        correto = input("Estão corretos?\n")
+        if correto == 's' or correto == 'sim' or correto == 'y' or correto == 'yes':
+            correto = True
+        else:
+            correto = False
+    return flavor
+
+def escolhas(pessoal,comidas,nomeDaComida,counter):
+    correto = False 
+    escolhas = [0] * comidas
+    temp = escolhas
     
-    correto = input("Estão corretos?\n")
-    if correto == 's' or correto == 'sim' or correto == 'y' or correto == 'yes':
-        correto = True
-    else:
-        correto = False
-correto = False
+    for i in range(comidas):
 
-print('\n\n\n\n\nAgora vamos ver os sabores de cada um!')
-for j in range(pessoas):
-    #^aqui vamos rodar pessoas
-    for i in range(sabores):
-        #^aqui vamos rodar cada sabor
-        tempflavor[i] = int(input("Pessoa n°["+str(j+1)+"] do sabor "+flavor[i]+" vai querer quantas?: "))
+        temp[i] = int(input("Pessoa n°["+str(counter+1)+"] do sabor "+nomeDaComida[i]+" vai querer quantas?: "))
         while correto == False:
-            #^aqui é a verificacao de cada sabor
+
             correto = input("Digitou corretamente?\n")
             if correto == 's' or correto == 'sim' or correto == 'y' or correto == 'yes':
                 correto = True
-                final[i] += tempflavor[i]
+                escolhas[i] += temp[i]
             else:
                 correto = False    
-        correto = False    
-                
-    for k in range(sabores):
-        print("\nVocê escolheu "+str(tempflavor[k])+"  do sabor "+ flavor[k])  
-    print("\n")
+        correto = False 
+    return escolhas
 
-for l in range(sabores):
-    print("Do sabor "+flavor[l]+" no total são: "+str(final[l]))
+def main():
+    pessoal = pessoas()
+    comidas = sabores()
+    nomeDaComida = nomeSabores(comidas)
 
+    print('\n\n\n\n\nAgora vamos ver os sabores de cada um!')
     
+    final =[0]*comidas 
+    for counter in range(pessoal):
+        passaDados = escolhas(pessoal,comidas,nomeDaComida,counter)
+        for i in range(comidas):
+            final[i] += passaDados[i]
+        
+
+    for j in range(comidas):
+        print("Do sabor "+nomeDaComida[j]+" no total são: "+str(final[j]))
+
+if __name__ == "__main__":
+    main()
